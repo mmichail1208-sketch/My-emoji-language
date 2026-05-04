2adf59da14d974b1d4e0b299d376796697ca7e88
mmichail1208-sketch <m.michail12.08@gmail.com>
2026-05-04 22:12:30 +0300

    Update interpret.py

--- interpret.py
+++ interpret.py
@@ -12,12 +12,12 @@ def test_output_char():
     assert interpret("👍 " * 72 + "🤖") == "H"
 def test_output_number():
     assert interpret("👍 " * 42 + "🔢") == "42"
-def test_input_number(x):
-    x.setattr('builtins.input', lambda _: "42")
+def test_input_number(monkeypatch):
+    monkeypatch.setattr('builtins.input', lambda _: "42")
     assert interpret("⏬ 🔢") == "42"
 def test_loop_start_end():
     assert interpret("👍 👍 👍 👍 👍 😂 👎 😭 🔢") == "0"
-def test_old_loop_start(x):
-    assert interpret("👍 👍 👍 ▶ 👎 ◀ 🔢") == "0" 
-def test_old_loop_end(x):
+def test_old_loop_start(monkeypatch):
+    assert interpret("👍 👍 👍 ▶ 👎 ◀ 🔢") == "0"
+def test_old_loop_end(monkeypatch):
     assert interpret("👍 👍 👍 ▶ 👎 ◀ 🔢") == "0"
