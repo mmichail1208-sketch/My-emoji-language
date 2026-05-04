0beae4b25f63fb39d477e9aa95c44d2892d968d3
mmichail1208-sketch <m.michail12.08@gmail.com>
2026-05-04 22:00:47 +0300

    Update interpret.py

--- interpret.py
+++ interpret.py
@@ -1,21 +1,23 @@
 import pytest
 from my import interpret
-def test_i():
+def test_increment():  # 👍
     assert interpret("👍 🤖") == chr(1)
-    assert interpret("👍" * 65 + "🤖") == "A"
-def test_d():
+def test_decrement():  # 👎
     assert interpret("👍" * 10 + "👎" * 3 + "🤖") == chr(7)
-def test_move_rt():
+def test_move_right():  # 👉
     assert interpret("👍" * 65 + "👉" + "👍" * 66 + "👈" + "🤖") == "B"
-def test_move_l():
+def test_move_left():  # 👈
     assert interpret("👉" + "👍" * 65 + "👈" + "🤖") == "A"
-def test_output_c():
+def test_output_char():  # 🤖
     assert interpret("👍" * 72 + "🤖") == "H"
-def test_output_n():
+def test_output_number():  # 🔢
     assert interpret("👍" * 42 + "🔢") == "42"
-def test_simple_lo():
+def test_input_number(x:  # ⏬
+    x.setattr('builtins.input', lambda _: "42")
+    assert interpret("⏬ 🔢") == "42"
+def test_loop_start_end():  # 😂 и 😭 (цикл)
     assert interpret("👍" * 5 + "😂 👎 😭 🔢") == "0"
-def test_nested_loo():
-    assert interpret("👍" * 3 + "😂 👉 👍" * 2 + "👈 👎 😭 👉 🔢") == "6"
-def test_face_output():
-    assert interpret("🍅") == "(❁◡❁)"
+def test_old_loop_start(monkeypatch):  # ▶ 
+    assert interpret("👍" * 3 + "▶ 👎 ◀ 🔢") == "0"
+def test_old_loop_end(monkeypatch):  # ◀ 
+    assert interpret("👍" * 3 + "▶ 👎 ◀ 🔢") == "0"
