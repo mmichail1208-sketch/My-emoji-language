f3addd6535755f0818fd957d01ca267eb129af9f
mmichail1208-sketch <m.michail12.08@gmail.com>
2026-05-04 21:52:45 +0300

    Update my.py

--- my.py
+++ my.py
@@ -1,24 +1,24 @@
 import sys
 
 def interpret(code):
-    tokens = code.split()  
+    tokens = code.replace('.', '').replace('!', '').split()
     
     memory = [0] * 30000
     pointer = 0
     pc = 0
     loops = {}
     stack = []
-    random_symbol = '(❁◡❁)'
-    output = ""  
     
-    # Поиск пар циклов с использованием лиц 😂 и 😭
+    # Сборка пар циклов для 😂 и 😭
     for i, token in enumerate(tokens):
-        if token == '😂':  # Начало цикла
+        if token == '😂':  # начало цикла
             stack.append(i)
-        elif token == '😭':  # Конец цикла
+        elif token == '😭':  # конец цикла
             start = stack.pop()
             loops[start] = i
             loops[i] = start
+    
+    output = ""  # Строка для вывода вместо списка
     
     while pc < len(tokens):
         command = tokens[pc]
@@ -35,15 +35,21 @@ def interpret(code):
             output += chr(memory[pointer])
         elif command == '🔢':  # Вывести число
             output += str(memory[pointer])
-        elif command == '🍅':  # Команда для мордочки
-            output += random_symbol
-        elif command == '😂':  # Цикл: если в ячейке 0, выйти из цикла
+        elif command == '⏬':  # Ввод числа в текущую ячейку
+            memory[pointer] = int(input())
+        elif command == '😂':  # начало цикла: если текущая ячейка = 0, перепрыгнуть
             if memory[pointer] == 0:
                 pc = loops[pc]
-        elif command == '😭':  # Конец цикла
+        elif command == '😭':  # конец цикла: если текущая ячейка ≠ 0
             if memory[pointer] != 0:
                 pc = loops[pc]
-
+        elif command == '▶':  
+            if memory[pointer] == 0:
+                pc = loops.get(pc, pc)
+        elif command == '◀': 
+            if memory[pointer] != 0:
+                pc = loops.get(pc, pc)
+        
         pc += 1
     
     return output
@@ -51,4 +57,3 @@ def interpret(code):
 if __name__ == "__main__":
     program = "👍" * 65 + "🤖"
     print(interpret(program))
-
