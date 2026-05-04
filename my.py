35cd0d5cc1c759c93db034510f426d1ce2269f54
mmichail1208-sketch <m.michail12.08@gmail.com>
2026-05-04 18:50:06 +0300

    Update my.py

--- my.py
+++ my.py
@@ -1,52 +1,54 @@
 import sys
 
 def interpret(code):
-    tokens = code.replace('.', '').replace('!', '').split()
+    tokens = code.split()  
     
     memory = [0] * 30000
     pointer = 0
     pc = 0
     loops = {}
     stack = []
+    random_simbol = ['(❁◡❁)']  # Морд
+    output = ""  
+    
     for i, token in enumerate(tokens):
-        if token == '😂':#wbrk
+        if token == '😂':  #Начало цикла
             stack.append(i)
-        elif token == '😭':#цикл
+        elif token == '😭':  #Конец цикла
             start = stack.pop()
             loops[start] = i
             loops[i] = start
-    random_simbol=['(❁◡❁)']
-    output = ""  # Строка для вывода вместо списка
     
     while pc < len(tokens):
         command = tokens[pc]
         
-        if command == '👍': # Увеличить значение
+        if command == '👍':  # Увеличить значение
             memory[pointer] += 1
-        elif command == '👎': #Уменьшить
+        elif command == '👎':  #Уменьшить
             memory[pointer] -= 1
-        elif command == '👉':#Сдвинуть указатель вправо
+        elif command == '👉':  #Сдвинуть указатель вправо
             pointer += 1
-        elif command == '👈':#Сдвинуть указатель влево
+        elif command == '👈':  #Сдвинуть указатель влево
             pointer -= 1
-        elif command == '🤖':
-            output = output + chr(memory[pointer])
-        elif command == '🔢':
-            output = output + str(memory[pointer]) + ' '
-        elif command == '🍅️':#Ввод числа в текущую ячейку
-            memory[pointer] = random_simbol
-        elif command == '😂':#Цикл: если в ячейке 0, выйти из цикла
+        elif command == '🤖':  #Вывести символ (ASCII)
+            output += chr(memory[pointer])
+        elif command == '🔢':  #Вывести число
+            output += str(memory[pointer]) + ' '
+        elif command == '🍅':  #Ввод числа в текущую ячейку (вывод мордочки)
+            output += ''.join(random_simbol)  # Вывод мордочки
+        elif command == '😂':  #Цикл: если в ячейке 0, выйти из цикла
             if memory[pointer] == 0:
                 pc = loops[pc]
-        elif command == '😭':#Конец цикла
+        elif command == '😭':  #Конец цикла
             if memory[pointer] != 0:
                 pc = loops[pc]
-        
+
         pc += 1
     
     if output.endswith(' '):
-        output = output[:-1]
+        output = output[:-1]  #Удалить проб
     return output
+
 if __name__ == "__main__":
     program = "👍" * 65 + "🤖"
     print(interpret(program))
